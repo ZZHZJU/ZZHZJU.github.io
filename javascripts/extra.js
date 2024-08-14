f704e6c40ebdbcdbfbad6812495b0c6d93bfccbf
ZZHZJU <2561694274@qq.com>
2024-08-14 14:37:45 +0800

    update

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -285,21 +285,3 @@ window.addEventListener('keydown', e => {
 
 requestFrame();
 //雪花
-
-// 评论
-<script src="https://giscus.app/client.js"
-        data-repo="ZZHZJU/ZZHZJU.github.io"
-        data-repo-id="R_kgDOLzHvOg"
-        data-category="General"
-        data-category-id="DIC_kwDOLzHvOs4ChnMd"
-        data-mapping="pathname"
-        data-strict="0"
-        data-reactions-enabled="1"
-        data-emit-metadata="0"
-        data-input-position="bottom"
-        data-theme="preferred_color_scheme"
-        data-lang="zh-CN"
-        crossorigin="anonymous"
-        async>
-</script>
-//评论
